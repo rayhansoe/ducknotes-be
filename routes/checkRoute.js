4aba1b8c1e467f6c8599f51d916244487f48bb5f
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-05-17 15:11:29 +0700

    add comment.

--- routes/checkRoute.js
+++ routes/checkRoute.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+// route for pinging the backend.
+
 router.route('/').get((req, res) => {
 	res.status(200).json({ isBackEndReady: true })
 })
